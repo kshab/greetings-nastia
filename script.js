e097fe87bae9a41bb535984dc6add2927b80a104
Konstantin Shabaliev <kvshabalev@gmail.com>
2023-12-22 00:00:07 +0200

    initial commit

---
+++ script.js
@@ -0,0 +1,37 @@
+const STATE = {
+  currentScreenIndex: 0,
+  screens: []
+};
+
+let interval = undefined;
+
+function run() {
+  console.log('Starting app...');
+  STATE.screens = document.querySelectorAll('.screen');
+
+  interval = setInterval(() => {
+    next();
+  }, 1000);
+}
+
+function next(isToDetails = false) {
+  const currentIndex = STATE.currentScreenIndex;
+  const nextIndex = currentIndex + 1;
+
+  if ((nextIndex + 1) >= STATE.screens.length && !isToDetails) {
+    clearInterval(interval);
+    return;
+  }
+
+  STATE.screens[currentIndex].classList.add('hidden');
+  STATE.screens[nextIndex].classList.remove('hidden');
+  STATE.currentScreenIndex++;
+}
+
+function toDetails() {
+  next(true);
+}
+
+function restart() {
+  location.reload();
+}
